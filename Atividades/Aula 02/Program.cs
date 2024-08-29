b98a4c30d757ed423961cbc66928800c4d7de170
Joao Vitor Tibes de Campos <424072@unoesc.lan>
2024-08-28 21:26:59 -0300

    Aula 28/08/2024

---
+++ Atividades/Aula 02/Program.cs
@@ -0,0 +1,23 @@
+﻿using Aula_02;
+
+Person person = new Person();
+Console.WriteLine(person.Name);
+Console.WriteLine(person.Age);
+
+Person person1 = new Person("Joao Vitor", 22);
+Console.WriteLine(person1.Name);
+Console.WriteLine(person1.Age);
+
+Person person2 = new Person();
+person2.Name = "Vladimir Ulyanov";
+person2.Age = 45;
+Console.WriteLine(person2.Name);
+Console.WriteLine(person2.Age);
+
+Person person3 = new Person()
+{
+    Name = "Lionel Messi",
+    Age = 35
+};
+Console.WriteLine(person3.Name);
+Console.WriteLine(person3.Age);
