ff2e6e76a46d24fd3813e46e2d6998f653de6534
Joao Vitor Tibes de Campos <424072@unoesc.lan>
2024-09-11 21:56:47 -0300

    Atv Completa

---
+++ Atividades/Aula 05 - 2 - ATV Pilhas/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+Console.Write("Digite uma palavra: ");
+string palavra = Console.ReadLine().Trim();
+
+bool ePalindromo = VerificarPalindromo(palavra);
+
+if (ePalindromo)
+{
+    Console.WriteLine($"A palavra '{palavra}' é um palíndromo.");
+}
+else
+{
+    Console.WriteLine($"A palavra '{palavra}' não é um palíndromo.");
+}
+
+static bool VerificarPalindromo(string palavra)
+{
+    Stack<char> pilha = new Stack<char>();
+
+    foreach (char c in palavra)
+    {
+        pilha.Push(c);
+    }
+    
+    string palavraInvertida = "";
+    while (pilha.Count > 0)
+    {
+        palavraInvertida += pilha.Pop();
+    }
+
+    return palavra.Equals(palavraInvertida, StringComparison.OrdinalIgnoreCase);
+}
---
+++ Atividades/Aula 05 - 2 - Pilhas/Program.cs
@@ -0,0 +1,38 @@
+﻿// Basicamente as pulhas trabalham com uma estrutura LIFO - Last In First Out;
+
+/* 
+
+Tres metodos importantes: 
+
+Push () -> Insere um elemento no topo da pilha;
+Pop () -> Remove um elemento do topo e o retorna;
+Peek () -> Retorna o elemento do topo sem removelo;
+
+*/
+
+// Invertendo as bolas:
+
+Stack<char> carcteres = new Stack<char>();
+foreach(char c in "DARCY AD MONTANHA")
+{
+    carcteres.Push(c);
+}
+
+// Desempilhando;
+
+string invertido = string.Empty;
+while(carcteres.Count > 0)
+{
+    invertido += carcteres.Pop();
+}
+
+Console.WriteLine(invertido);
+
+/*
+
+ATIVIDADE:
+
+Utilizando o conceito de pilha(stack) com os metodos pop e push escreva um programa que leia uma entrada do usua e identifique se a palavra 
+informada é ou não um palindromo.
+
+*/
