bf0e8c9d987553c9860a0899e316273b60c9ec48
Joao Vitor Tibes de Campos <424072@unoesc.lan>
2024-10-09 21:41:00 -0300

    Aula

---
+++ Atividades/Aula 08 - BubbleSort/Program.cs
@@ -0,0 +1,19 @@
+﻿using Aula_08___BubbleSort;
+
+int[] inteiros = [0, 12, 7, 1, 2, 8];
+
+Console.Write('[');
+foreach(int i in inteiros)
+{
+    Console.Write($"{i} ");
+}
+Console.Write(']');
+
+Aula_08___BubbleSort.BubbleSort.Sort<int>(inteiros);
+
+Console.Write('[');
+foreach(int i in inteiros)
+{
+    Console.Write($"{i} ");
+}
+Console.Write(']');
